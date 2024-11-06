4c058e9476c1e1075f8e4234e94d0fad99146e56
VolodymirM <volodymir.moskalyuk@gmail.com>
2024-11-06 02:42:41 +0200

    Composite structural design pattern

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -14,27 +14,15 @@ public class Menu {
     private static JPanel panel;
     private static JFrame frame;
 
-    private static JLabel loanLabel;
+    private static JLabel label; // Composite structural design pattern
+    
     private static JTextField loanText;
-    private static JLabel euroLabel;
-    
-    private static JLabel percentLabel;
     private static JTextField percentText;
-    private static JLabel percentSymbol;
-    
-    private static JLabel returnLabel;
     private static JTextField yearText;
-    private static JLabel yearLabel;
     private static JTextField monthText;
-    private static JLabel monthLabel;
-    
     private static JCheckBox deferCheckBox;
-    private static JLabel deferMonthLabel;
     private static JTextField deferMonthText;
-    private static JLabel deferDurationLabel;
     private static JTextField deferDurationText;
-    
-    private static JLabel repaymentLabel;
     private static DefaultListModel<String> repaymentModel;
     private static JList<String> repaymentList;
 
@@ -77,69 +65,69 @@ public class Menu {
         panel.setLayout(null);
         
         // Loan amount
-        loanLabel = new JLabel("Loan amount: ");
-        loanLabel.setBounds(10, 10, 80, 25);
-        panel.add(loanLabel);
+        label = new JLabel("Loan amount: ");
+        label.setBounds(10, 10, 80, 25);
+        panel.add(label);
 
         loanText = new JTextField("0.00");
         loanText.setBounds(95, 10, 80, 25);
         panel.add(loanText);
 
-        euroLabel = new JLabel("€");
-        euroLabel.setBounds(180, 10, 25, 25);
-        panel.add(euroLabel);
+        label = new JLabel("€");
+        label.setBounds(180, 10, 25, 25);
+        panel.add(label);
 
         // Annual interest
-        percentLabel = new JLabel("Annual interest: ");
-        percentLabel.setBounds(234, 10, 100, 25);
-        panel.add(percentLabel);
+        label = new JLabel("Annual interest: ");
+        label.setBounds(234, 10, 100, 25);
+        panel.add(label);
 
         percentText = new JTextField("0.00");
         percentText.setBounds(330, 10, 40, 25);
         panel.add(percentText);
 
-        percentSymbol = new JLabel("%");
-        percentSymbol.setBounds(375, 10, 25, 25);
-        panel.add(percentSymbol);
+        label = new JLabel("%");
+        label.setBounds(375, 10, 25, 25);
+        panel.add(label);
 
         // Return term
-        returnLabel = new JLabel("Return term: ");
-        returnLabel.setBounds(10,40,80,25);
-        panel.add(returnLabel);
+        label = new JLabel("Return term: ");
+        label.setBounds(10,40,80,25);
+        panel.add(label);
 
         yearText = new JTextField("0");
         yearText.setBounds(95, 40, 25, 25);
         panel.add(yearText);
 
-        yearLabel = new JLabel("years");
-        yearLabel.setBounds(125, 40, 40, 25);
-        panel.add(yearLabel);
+        label = new JLabel("years");
+        label.setBounds(125, 40, 40, 25);
+        panel.add(label);
 
         monthText = new JTextField("0");
         monthText.setBounds(165, 40, 25, 25);
         panel.add(monthText);
 
-        monthLabel = new JLabel("months");
-        monthLabel.setBounds(195, 40, 45, 25);
-        panel.add(monthLabel);
+        label = new JLabel("months");
+        label.setBounds(195, 40, 45, 25);
+        panel.add(label);
 
         // Defer loan payment
         deferCheckBox = new JCheckBox("Defer loan payment");
         deferCheckBox.setBounds(10, 70, 135, 25);
         panel.add(deferCheckBox);
 
-        deferMonthLabel = new JLabel("Deferment month: ");
-        deferMonthLabel.setBounds(220, 70, 105, 25);
-        panel.add(deferMonthLabel);
+        label = new JLabel("Deferment month: ");
+        label.setBounds(220, 70, 105, 25);
+        panel.add(label);
 
         deferMonthText = new JTextField("0");
         deferMonthText.setBounds(330, 70, 40, 25);
         deferMonthText.setEditable(false);
         panel.add(deferMonthText);
 
-        deferDurationLabel = new JLabel("Deferment duration: ");
-        deferDurationLabel.setBounds(208, 100, 120, 25);
-        panel.add(deferDurationLabel);
+        label = new JLabel("Deferment duration: ");
+        label.setBounds(208, 100, 120, 25);
+        panel.add(label);
 
         deferDurationText = new JTextField("0");
         deferDurationText.setBounds(330, 100, 40, 25);
@@ -147,9 +135,9 @@ public class Menu {
         panel.add(deferDurationText);
         
         // Repayment schedule
-        repaymentLabel = new JLabel("Repayment schedule: ");
-        repaymentLabel.setBounds(10, 145, 125, 25);
-        panel.add(repaymentLabel);
+        label = new JLabel("Repayment schedule: ");
+        label.setBounds(10, 145, 125, 25);
+        panel.add(label);
         
         repaymentModel = new DefaultListModel<>();
         repaymentModel.addElement("Annuity");
